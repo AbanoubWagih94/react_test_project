3bfb2c159198153120568b3de07ae526ad743c9e
Abanoub <abanoubwagi94@gmail.com>
2022-07-11 19:31:21 +0200

    functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component, createElement } from 'react';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>Welcome React</h1>
+      <Person name="abnaoub" age="28"><p>My Hobbies: Football</p></Person>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'welcome react'))
