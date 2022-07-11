65a9b1b95c1e925bbdf607a376c219897dac73e0
Abanoub <abanoubwagi94@gmail.com>
2022-07-12 01:16:41 +0200

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React, { Component, createElement } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1>Welcome React</h1>
-      <Person name="abnaoub" age="28"><p>My Hobbies: Football</p></Person>
-      </div>
-    );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'welcome react'))
+
+const App = props => {
+
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'abanoub', 'age': 28 },
+      { name: 'sandy', 'age': 25 },
+    ]
+  });
+  const [otherState, setOtherState] = useState('some other value');
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: 'abanoub wagih', 'age': 28 },
+        { name: 'sandy', 'age': 25 },
+      ]
+    });
   }
+
+  return (
+    <div className="App">
+      <h1>Welcome React</h1>
+      <button onClick={switchNameHandler}> Click here</button>
+      <Person name={personsState.persons[0].name}
+       age={personsState.persons[0].age}>
+        <p>My Hobbies: Football</p>
+        </Person>
+
+      <Person name={personsState.persons[1].name} 
+      age={personsState.persons[1].age}
+      >
+      </Person>
+    </div>
+  );
 }
 
+
 export default App;
+
